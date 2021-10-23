7de65dc4e66f4c42e7783f64e50ed0459c127955
Schalk van Dyk <schalkvandyk1979@gmail.com>
2021-10-23 10:17:13 +0200

    add basic functionality to gameInfo sections

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+const nameInputDisplay = document.querySelector(".nameInputDisplay");
+const playerNameDislpay = document.querySelector(".playerNameDisplay");
+const gameDifficultyCheckboxes = document.querySelector(
+	".gameDifficultyCheckboxes"
+);
+const loadGameBtn = document.querySelector(".loadGameBtn");
+const resetAndShuffleBtn = document.querySelector(".resetAndShuffleBtn");
+const newPlayerBtn = document.querySelector(".newPlayerBtn");
+
+loadGameBtn.addEventListener("click", function() {
+	playerNameDislpay.textContent = nameInputDisplay.value;
+	nameInputDisplay.style.display = "none";
+	gameDifficultyCheckboxes.style.display = "none";
+	loadGameBtn.style.display = "none";
+	resetAndShuffleBtn.style.display = "block";
+	newPlayerBtn.style.display = "block";
+});
+
+newPlayerBtn.addEventListener("click", function() {
+	playerNameDislpay.textContent = "___enter new player name";
+	nameInputDisplay.style.display = "block";
+	gameDifficultyCheckboxes.style.display = "block";
+	loadGameBtn.style.display = "block";
+	resetAndShuffleBtn.style.display = "none";
+	newPlayerBtn.style.display = "none";
+});

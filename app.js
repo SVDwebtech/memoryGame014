061f68e389d08ec87c3b80b0d292881486c955f3
Schalk van Dyk <schalkvandyk1979@gmail.com>
2021-10-23 17:01:07 +0200

    add basic card flip functionality
        *   card flip functionality not finished
        *   problem 1 - click same card twice and it is hidden
        *   problem 2 - no pictures on back of cards yet

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const gameDifficultyCheckboxes = document.querySelector(
 const loadGameBtn = document.querySelector(".loadGameBtn");
 const resetAndShuffleBtn = document.querySelector(".resetAndShuffleBtn");
 const newPlayerBtn = document.querySelector(".newPlayerBtn");
+const cardDisplay = document.querySelectorAll(".cardDisplay");
+const cardContainer = document.querySelector(".card-container");
+const cardSideDisplay = document.querySelectorAll(".card__side");
 
 loadGameBtn.addEventListener("click", function() {
 	playerNameDislpay.textContent = nameInputDisplay.value;
@@ -24,3 +27,72 @@ newPlayerBtn.addEventListener("click", function() {
 	resetAndShuffleBtn.style.display = "none";
 	newPlayerBtn.style.display = "none";
 });
+
+let clickCount = 0;
+let counter = 0;
+let compareCardOneText = "";
+let compareCardTwoText = "";
+let cardNumOne = 0;
+let cardNumTwo = 0;
+for (let i = 0; i < cardDisplay.length; i++)
+	cardDisplay[i].addEventListener("click", function() {
+		clickCount++;
+		console.log("clickCount = " + clickCount);
+		if (counter <= 1) {
+			cardSideDisplay[i].style.backgroundColor = "red";
+			cardSideDisplay[i].classList.toggle("rotateCard");
+		}
+		if (counter === 0) {
+			compareCardOneText = cardSideDisplay[i].textContent;
+			console.log(compareCardOneText);
+			cardNumOne = i;
+			console.log(cardNumOne);
+		}
+		if (counter === 1) {
+			compareCardTwoText = cardSideDisplay[i].textContent;
+			console.log(compareCardTwoText);
+			cardNumTwo = i;
+			console.log(cardNumTwo);
+			if (compareCardOneText === compareCardTwoText) {
+				setTimeout(function() {
+					cardDisplay[cardNumOne].style.visibility = "hidden";
+					cardDisplay[cardNumTwo].style.visibility = "hidden";
+				}, 1000);
+			}
+
+			setTimeout(function() {
+				counter = 0;
+				compareCardOneText = "";
+				compareCardTwoText = "";
+				cardSideDisplay[cardNumOne].style.backgroundColor =
+					"rgb(35, 35, 35)";
+				cardSideDisplay[cardNumTwo].style.backgroundColor =
+					"rgb(35, 35, 35)";
+				cardSideDisplay[cardNumOne].classList.toggle("rotateCard");
+				cardSideDisplay[cardNumTwo].classList.toggle("rotateCard");
+				cardNumOne = 0;
+				cardNumTwo = 0;
+				console.log("Counter should be -1 = " + counter);
+				console.log(
+					"CompareCardOneText should be empty = " +
+						'"' +
+						compareCardOneText +
+						'"'
+				);
+				console.log(
+					"CompareCardTwoText should be empty = " +
+						'"' +
+						compareCardTwoText +
+						'"'
+				);
+				console.log(
+					"CardNumOne should be empty = " + '"' + cardNumOne + '"'
+				);
+				console.log(
+					"CardNumTwo should be empty = " + '"' + cardNumTwo + '"'
+				);
+			}, 2000);
+		}
+		counter++;
+		console.log("Counter is now = " + counter);
+	});

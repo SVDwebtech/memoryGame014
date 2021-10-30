19cfc77979b61f382c2f0df50a625da2bfaf2568
Schalk van Dyk <schalkvandyk1979@gmail.com>
2021-10-30 16:28:17 +0200

    move game logic into load new game functionality

--- app.js
+++ app.js
@@ -12,13 +12,144 @@ const cardSideDisplayFront = document.querySelectorAll(".card__side--front");
 const cardSideDisplayBack = document.querySelectorAll(".card__side--back");
 const counterDisplay = document.querySelector(".counter--display");
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Timer Dom Variables
+const minhand = document.querySelector(".minhand");
+const sechand = document.querySelector(".sechand");
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Timer Game Variables
+let minclick = 0;
+let click = 0;
+// let isGameLoaded = false;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Game Logic Variables
+let clickCount = 0;
+let clickCounter = 0;
+let counter = 0;
+let compareCardOneText = "";
+let compareCardTwoText = "";
+let cardNumOne = 0;
+let cardNumTwo = 0;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Load New Game Logic
 loadGameBtn.addEventListener("click", function() {
+	// isGameLoaded = true;
+	// console.log(isGameLoaded);
+	// if (isGameLoaded) {
+	setInterval(() => {
+		click++;
+		// console.log(click);
+		if (click < 60) {
+			if (click < 10) {
+				sechand.textContent = "0" + click;
+			}
+			else {
+				sechand.textContent = click;
+			}
+		}
+		else if (click === 60) {
+			minclick++;
+			minhand.textContent = "0" + minclick;
+			sechand.textContent = "00";
+			click = 0;
+		}
+	}, 1000);
+	// }
 	playerNameDislpay.textContent = nameInputDisplay.value;
 	nameInputDisplay.style.display = "none";
 	gameDifficultyCheckboxes.style.display = "none";
 	loadGameBtn.style.display = "none";
 	resetAndShuffleBtn.style.display = "block";
 	newPlayerBtn.style.display = "block";
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// Game Logic
+	for (let i = 0; i < cardDisplay.length; i++)
+		cardDisplay[i].addEventListener("click", function() {
+			clickCount++;
+			console.log("clickCount = " + clickCount);
+			if (counter <= 1) {
+				// cardSideDisplay[i].style.backgroundColor = "red";
+				cardSideDisplayFront[i].classList.toggle("rotateCard180");
+				cardSideDisplayBack[i].classList.toggle("rotateCard0");
+			}
+			if (counter === 0) {
+				compareCardOneText = cardSideDisplayBack[i].textContent;
+				console.log(compareCardOneText);
+				cardNumOne = i;
+				console.log(cardNumOne);
+			}
+			if (counter === 1) {
+				clickCounter += counter;
+				if (clickCounter <= 9) {
+					counterDisplay.textContent = "00" + clickCounter;
+				}
+				else if (clickCounter <= 99) {
+					counterDisplay.textContent = "0" + clickCounter;
+				}
+				else {
+					counterDisplay.textContent = clickCounter;
+				}
+				compareCardTwoText = cardSideDisplayBack[i].textContent;
+				console.log(compareCardTwoText);
+				cardNumTwo = i;
+				console.log(cardNumTwo);
+				if (compareCardOneText === compareCardTwoText) {
+					setTimeout(function() {
+						cardDisplay[cardNumOne].style.visibility = "hidden";
+						cardDisplay[cardNumTwo].style.visibility = "hidden";
+					}, 1000);
+				}
+
+				setTimeout(function() {
+					counter = 0;
+					compareCardOneText = "";
+					compareCardTwoText = "";
+					// cardSideDisplay[cardNumOne].style.backgroundColor =
+					// 	"rgb(35, 35, 35)";
+					// cardSideDisplay[cardNumTwo].style.backgroundColor =
+					// 	"rgb(35, 35, 35)";
+					cardSideDisplayFront[cardNumOne].classList.toggle(
+						"rotateCard180"
+					);
+					cardSideDisplayBack[cardNumOne].classList.toggle(
+						"rotateCard0"
+					);
+					cardSideDisplayFront[cardNumTwo].classList.toggle(
+						"rotateCard180"
+					);
+					cardSideDisplayBack[cardNumTwo].classList.toggle(
+						"rotateCard0"
+					);
+					cardNumOne = 0;
+					cardNumTwo = 0;
+					console.log("Counter should be -1 = " + counter);
+					console.log(
+						"CompareCardOneText should be empty = " +
+							'"' +
+							compareCardOneText +
+							'"'
+					);
+					console.log(
+						"CompareCardTwoText should be empty = " +
+							'"' +
+							compareCardTwoText +
+							'"'
+					);
+					console.log(
+						"CardNumOne should be empty = " + '"' + cardNumOne + '"'
+					);
+					console.log(
+						"CardNumTwo should be empty = " + '"' + cardNumTwo + '"'
+					);
+				}, 2000);
+			}
+			counter++;
+			console.log("Counter is now = " + counter);
+		});
 });
 
 newPlayerBtn.addEventListener("click", function() {
@@ -29,117 +160,3 @@ newPlayerBtn.addEventListener("click", function() {
 	resetAndShuffleBtn.style.display = "none";
 	newPlayerBtn.style.display = "none";
 });
-
-let clickCount = 0;
-let clickCounter = 0;
-let counter = 0;
-let compareCardOneText = "";
-let compareCardTwoText = "";
-let cardNumOne = 0;
-let cardNumTwo = 0;
-for (let i = 0; i < cardDisplay.length; i++)
-	cardDisplay[i].addEventListener("click", function() {
-		clickCount++;
-		console.log("clickCount = " + clickCount);
-		if (counter <= 1) {
-			// cardSideDisplay[i].style.backgroundColor = "red";
-			cardSideDisplayFront[i].classList.toggle("rotateCard180");
-			cardSideDisplayBack[i].classList.toggle("rotateCard0");
-		}
-		if (counter === 0) {
-			compareCardOneText = cardSideDisplayBack[i].textContent;
-			console.log(compareCardOneText);
-			cardNumOne = i;
-			console.log(cardNumOne);
-		}
-		if (counter === 1) {
-			clickCounter += counter;
-			if (clickCounter <= 9) {
-				counterDisplay.textContent = "00" + clickCounter;
-			}
-			else if (clickCounter <= 99) {
-				counterDisplay.textContent = "0" + clickCounter;
-			}
-			else {
-				counterDisplay.textContent = clickCounter;
-			}
-			compareCardTwoText = cardSideDisplayBack[i].textContent;
-			console.log(compareCardTwoText);
-			cardNumTwo = i;
-			console.log(cardNumTwo);
-			if (compareCardOneText === compareCardTwoText) {
-				setTimeout(function() {
-					cardDisplay[cardNumOne].style.visibility = "hidden";
-					cardDisplay[cardNumTwo].style.visibility = "hidden";
-				}, 1000);
-			}
-
-			setTimeout(function() {
-				counter = 0;
-				compareCardOneText = "";
-				compareCardTwoText = "";
-				// cardSideDisplay[cardNumOne].style.backgroundColor =
-				// 	"rgb(35, 35, 35)";
-				// cardSideDisplay[cardNumTwo].style.backgroundColor =
-				// 	"rgb(35, 35, 35)";
-				cardSideDisplayFront[cardNumOne].classList.toggle(
-					"rotateCard180"
-				);
-				cardSideDisplayBack[cardNumOne].classList.toggle("rotateCard0");
-				cardSideDisplayFront[cardNumTwo].classList.toggle(
-					"rotateCard180"
-				);
-				cardSideDisplayBack[cardNumTwo].classList.toggle("rotateCard0");
-				cardNumOne = 0;
-				cardNumTwo = 0;
-				console.log("Counter should be -1 = " + counter);
-				console.log(
-					"CompareCardOneText should be empty = " +
-						'"' +
-						compareCardOneText +
-						'"'
-				);
-				console.log(
-					"CompareCardTwoText should be empty = " +
-						'"' +
-						compareCardTwoText +
-						'"'
-				);
-				console.log(
-					"CardNumOne should be empty = " + '"' + cardNumOne + '"'
-				);
-				console.log(
-					"CardNumTwo should be empty = " + '"' + cardNumTwo + '"'
-				);
-			}, 2000);
-		}
-		counter++;
-		console.log("Counter is now = " + counter);
-	});
-
-/////////////////////////////////////////////////////////////
-// TIMER
-/////////////////////////////////////////////////////////////
-const minhand = document.querySelector(".minhand");
-const sechand = document.querySelector(".sechand");
-
-let minclick = 0;
-let click = 0;
-setInterval(() => {
-	click++;
-	// console.log(click);
-	if (click < 60) {
-		if (click < 10) {
-			sechand.textContent = "0" + click;
-		}
-		else {
-			sechand.textContent = click;
-		}
-	}
-	else if (click === 60) {
-		minclick++;
-		minhand.textContent = "0" + minclick;
-		sechand.textContent = "00";
-		click = 0;
-	}
-}, 1000);

be40cff773fe9f5dfea471f5f8a79ffed7f9effd
Schalk van Dyk <schalkvandyk1979@gmail.com>
2021-11-01 13:56:12 +0200

    refactor app.js

--- app.js
+++ app.js
@@ -1,83 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// DOM VARIABLES
+
+// game input variables
 const nameInputDisplay = document.querySelector(".nameInputDisplay");
-const playerNameDislpay = document.querySelector(".playerNameDisplay");
 const gameDifficultyCheckboxes = document.querySelector(
 	".gameDifficultyCheckboxes"
 );
+
+// player info display variables
+const playerNameDislpay = document.querySelector(".playerNameDisplay");
+const counterDisplay = document.querySelector(".counter--display");
+const minhand = document.querySelector(".minhand");
+const sechand = document.querySelector(".sechand");
+
+// game button variables
 const loadGameBtn = document.querySelector(".loadGameBtn");
 const resetAndShuffleBtn = document.querySelector(".resetAndShuffleBtn");
 const newPlayerBtn = document.querySelector(".newPlayerBtn");
+
+// card display variables
 const cardDisplay = document.querySelectorAll(".cardDisplay");
 const cardContainer = document.querySelector(".card-container");
 const cardSideDisplayFront = document.querySelectorAll(".card__side--front");
 const cardSideDisplayBack = document.querySelectorAll(".card__side--back");
-const counterDisplay = document.querySelector(".counter--display");
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-// Timer Dom Variables
-const minhand = document.querySelector(".minhand");
-const sechand = document.querySelector(".sechand");
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
-// Timer Game Variables
+// GAME LOGIC VARIABLES
+
+// timer variables
 let minclick = 0;
 let click = 0;
-// let isGameLoaded = false;
+let intervalID;
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-// Game Logic Variables
+// counter variables
 let clickCount = 0;
 let clickCounter = 0;
 let counter = 0;
+
+// card comparisson variables
 let compareCardOneText = "";
 let compareCardTwoText = "";
 let cardNumOne = 0;
 let cardNumTwo = 0;
-let cardCompareCounter = 5;
-let intervalID;
+let cardPairsHiddenCounter = 5;
 let cardFront;
 let cardBack;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // Load New Game Logic
-loadGameBtn.addEventListener("click", function() {
-	startTimer();
-	playerNameDislpay.textContent = nameInputDisplay.value;
-	nameInputDisplay.value = "";
-	nameInputDisplay.style.display = "none";
-	gameDifficultyCheckboxes.style.display = "none";
-	loadGameBtn.style.display = "none";
-	resetAndShuffleBtn.style.display = "block";
-	newPlayerBtn.style.display = "block";
-
-	///////////////////////////////////////////////////////////////////////////////////////////////
-	// Game Logic
-	for (let i = 0; i < cardDisplay.length; i++)
-		cardDisplay[i].addEventListener("click", function() {
-			clickCount++;
-			cardFront = cardSideDisplayFront[i];
-			cardBack = cardSideDisplayBack[i];
-			// flip card to back when clicked
-			console.log("just before rotate card: counter = " + counter);
-			rotateCard();
-
-			if (counter === 0) {
-				compareCardOneText = cardSideDisplayBack[i].textContent;
-				cardNumOne = i;
-			}
-
-			if (counter === 1) {
-				flipCounter();
-				compareCardTwoText = cardSideDisplayBack[i].textContent;
-				cardNumTwo = i;
-				// compare cards and if equal hide them
-				compareCards();
-				// flip both cards to front after compared
-				rotateBothCards();
-			}
-			counter++;
-			console.log("at end of game logic: Counter = " + counter);
-		});
-});
+loadGameBtn.addEventListener("click", gameLogic);
 
 newPlayerBtn.addEventListener("click", function() {
 	playerNameDislpay.textContent = "___enter new player name";
@@ -123,11 +94,65 @@ newPlayerBtn.addEventListener("click", function() {
 	counterDisplay.textContent = "000";
 });
 
+//newPlayerBtn.removeEventListener("click", gameLogic);
+
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // Game Functions
 
+// Game Logic
+function gameLogic() {
+	console.log("loadGameBtn clicked");
+	// start timer display
+	startTimer();
+	console.log("player name captured and display other buttons");
+	// capture name and reset input value
+	playerNameDislpay.textContent = nameInputDisplay.value;
+	nameInputDisplay.value = "";
+	// hide name input and gamedifficulty settings and display other game buttons
+	nameInputDisplay.style.display = "none";
+	gameDifficultyCheckboxes.style.display = "none";
+	loadGameBtn.style.display = "none";
+	resetAndShuffleBtn.style.display = "block";
+	newPlayerBtn.style.display = "block";
+
+	// add click listener to each card
+	console.log("eventlistener added to each card");
+	for (let i = 0; i < cardDisplay.length; i++) {
+		cardDisplay[i].addEventListener("click", function() {
+			clickCount++;
+			cardFront = cardSideDisplayFront[i];
+			cardBack = cardSideDisplayBack[i];
+			// flip card to back when clicked
+			console.log("just before rotate card: counter = " + counter);
+			// rotate card to back when clicked
+			console.log("rotate card to back");
+			rotateCard();
+
+			if (counter === 0) {
+				compareCardOneText = cardSideDisplayBack[i].textContent;
+				cardNumOne = i;
+			}
+
+			if (counter === 1) {
+				flipCounter();
+				compareCardTwoText = cardSideDisplayBack[i].textContent;
+				cardNumTwo = i;
+				// compare cards and if equal hide them
+				console.log("compare the pair of cards");
+				compareCards();
+				// rotate both cards to front after compared
+				console.log("rotate the pair of cards to front");
+				rotateBothCards();
+			}
+			counter++;
+			console.log("at end of game logic: Counter = " + counter);
+		});
+	}
+}
+
 // Start Timer Function
 function startTimer() {
+	console.log("timer started");
 	intervalID = setInterval(() => {
 		click++;
 
@@ -150,8 +175,8 @@ function startTimer() {
 
 // Stop Timer Function
 function stopTimer() {
-	cardCompareCounter--;
-	if (cardCompareCounter === 0) {
+	cardPairsHiddenCounter--;
+	if (cardPairsHiddenCounter === 0) {
 		clearInterval(intervalID);
 	}
 }
@@ -167,8 +192,8 @@ function rotateCard() {
 
 // Rotate Both Cards Function
 function rotateBothCards() {
+	counter = -1;
 	setTimeout(function() {
-		counter = 0;
 		compareCardOneText = "";
 		compareCardTwoText = "";
 
